095bf786144b432018a7141da3049e7dbc2dca6b
Paul <97813342+Paul1404@users.noreply.github.com>
2023-09-06 21:55:30 +0200

    Added Background + XML Validation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,91 +1,78 @@
 ﻿using Microsoft.Win32;
-using System.Collections.Generic;
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 using System.Windows;
-using System;
-using System.Linq;
+using System.Windows.Controls;
 
 namespace OfficeInstaller
 {
     public partial class MainWindow : Window
     {
         private const string SetupUrl = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16626-20148.exe";
-        private readonly string _localSetupPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "setup.exe");
-
-        public List<string> XmlFiles { get; set; } = new List<string>();
+        private readonly string _localSetupPath;
+        private static readonly HttpClient _httpClient = new HttpClient();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _localSetupPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "setup.exe");
             _ = DownloadLatestSetupAsync();
             PopulateXmlFiles();
         }
 
-        public class XmlFile
-        {
-            public string FileName { get; set; }
-            public string FullPath { get; set; }
-
-            public XmlFile(string fileName, string fullPath)
-            {
-                FileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
-                FullPath = fullPath ?? throw new ArgumentNullException(nameof(fullPath));
-            }
-
-            public static XmlFile Placeholder => new XmlFile("Select a Configuration XML...", "");
-
-            public override string ToString()
-            {
-                return FileName;
-            }
-        }
-
-
         private void PopulateXmlFiles()
         {
-            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string[] xmlFiles = Directory.GetFiles(appDirectory, "*.xml");
+            var xmlFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.xml")
+                .Select(file => new XmlFile(Path.GetFileName(file), file))
+                .ToList();
 
-            // Ensure the placeholder is the first item.
-            if (XmlFilesComboBox.Items.Count == 0 || (XmlFilesComboBox.Items[0] as XmlFile)?.FileName != XmlFile.Placeholder.FileName)
-            {
-                XmlFilesComboBox.Items.Insert(0, XmlFile.Placeholder);
-            }
-
-            // Add the XML files.
-            foreach (var xmlFile in xmlFiles)
-            {
-                XmlFilesComboBox.Items.Add(new XmlFile(Path.GetFileName(xmlFile), xmlFile));
-            }
-
-            // Set the default item as selected.
+            XmlFilesComboBox.Items.Add(XmlFile.Placeholder);
+            xmlFiles.ForEach(xml => XmlFilesComboBox.Items.Add(xml));
             XmlFilesComboBox.SelectedIndex = 0;
         }
 
+        private async Task DownloadLatestSetupAsync()
+        {
+            if (File.Exists(_localSetupPath)) return;
 
+            try
+            {
+                byte[] setupData = await _httpClient.GetByteArrayAsync(SetupUrl);
+                await File.WriteAllBytesAsync(_localSetupPath, setupData);
 
+                MessageBox.Show("Setup was successfully downloaded.");
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Failed to download the setup. Please check your internet connection.");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Failed to write the setup file to disk. Ensure you have sufficient permissions and disk space.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}");
+            }
+        }
 
         private void XmlFilesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (XmlFilesComboBox.SelectedItem is XmlFile selectedXmlFile)
             {
-                if (selectedXmlFile == XmlFile.Placeholder)
-                {
-                    XmlPathTextBox.Text = ""; // Reset/clear the textbox.
-                }
-                else
-                {
-                    XmlPathTextBox.Text = selectedXmlFile.FullPath;
-                }
+                XmlPathTextBox.Text = selectedXmlFile != XmlFile.Placeholder ? selectedXmlFile.FullPath : string.Empty;
             }
         }
 
-
-
+        private void XmlPathTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateXml(XmlPathTextBox.Text);
+        }
 
         private void ChooseXmlButton_Click(object sender, RoutedEventArgs e)
         {
@@ -97,39 +84,34 @@ namespace OfficeInstaller
             if (openFileDialog.ShowDialog() == true)
             {
                 XmlPathTextBox.Text = openFileDialog.FileName;
+                ValidateXml(XmlPathTextBox.Text);
             }
         }
 
-        private async Task DownloadLatestSetupAsync()
+        private void ValidateXml(string path)
         {
-            using var client = new HttpClient();
-            try
+            if (XmlUtilities.IsValidXml(path))
             {
-                var response = await client.GetAsync(SetupUrl);
-                response.EnsureSuccessStatusCode();
-
-                var bytes = await response.Content.ReadAsByteArrayAsync();
-                await File.WriteAllBytesAsync(_localSetupPath, bytes);
+                ValidationCheckmark.Visibility = Visibility.Visible;
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show($"An error occurred: {ex.Message}");
+                ValidationCheckmark.Visibility = Visibility.Collapsed;
+                MessageBox.Show("The chosen XML is not valid.");
             }
         }
 
         private void InstallOfficeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(XmlPathTextBox.Text) || XmlPathTextBox.Text == "Select an XML file...")
+            if (string.IsNullOrEmpty(XmlPathTextBox.Text) || XmlPathTextBox.Text == XmlFile.Placeholder.FullPath)
             {
                 MessageBox.Show("Please select an XML configuration file first.");
                 return;
             }
 
-            string arguments = $"/configure \"{XmlPathTextBox.Text}\"";
-
             try
             {
-                Process.Start(_localSetupPath, arguments);
+                Process.Start(_localSetupPath, $"/configure \"{XmlPathTextBox.Text}\"");
             }
             catch (Exception ex)
             {
@@ -139,9 +121,40 @@ namespace OfficeInstaller
 
         private void ShowExeFolderButton_Click(object sender, RoutedEventArgs e)
         {
-            string exeFolderPath = AppDomain.CurrentDomain.BaseDirectory;
-            Process.Start("explorer.exe", exeFolderPath);
+            Process.Start("explorer.exe", AppDomain.CurrentDomain.BaseDirectory);
+        }
+    }
+
+    public class XmlFile
+    {
+        public string FileName { get; }
+        public string FullPath { get; }
+
+        public XmlFile(string fileName, string fullPath)
+        {
+            FileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
+            FullPath = fullPath ?? throw new ArgumentNullException(nameof(fullPath));
         }
 
+        public static XmlFile Placeholder { get; } = new XmlFile("Select a Configuration XML...", "");
+
+        public override string ToString() => FileName;
+    }
+
+    public static class XmlUtilities
+    {
+        public static bool IsValidXml(string path)
+        {
+            try
+            {
+                var xmlDocument = new System.Xml.XmlDocument();
+                xmlDocument.Load(path);
+                return true; // If XML is successfully loaded
+            }
+            catch
+            {
+                return false; // Any error in parsing will result in an invalid XML
+            }
+        }
     }
 }
